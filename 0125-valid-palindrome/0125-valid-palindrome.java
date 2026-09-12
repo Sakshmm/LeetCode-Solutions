5ba3e8754579f09c4d39faa66be394e390206933
Saksham Thakur <sakshamthakur543@gmail.com>
2026-09-12 21:54:23 +0530

    Time: 2 ms (99.31%), Space: 44.2 MB (73.24%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left= 0;
-        int right= s.length()-1;
-
+        int left=0;
+        int right=s.length()-1;
+        
         while(left<right){
             if(!Character.isLetterOrDigit(s.charAt(left))){
                 left++;
@@ -12,13 +12,14 @@ class Solution {
                 right--;
                 continue;
             }
-            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+
+            if(Character.toLowerCase(s.charAt(left)) != 
+            Character.toLowerCase(s.charAt(right))){
                 return false;
             }
             left++;
             right--;
         }
         return true;
-
     }
 }
